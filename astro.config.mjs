8abbc69c328b48721963eba14bf269150dca9814
Michael Zephyr <mzephyr@nvidia.com>
2024-04-29 14:41:19 -0700

    Test GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://zephyrie.github.io/nvflare-web/",
+  site: "https://zephyrie.github.io",
+  base: "nvflare-web",
   integrations: [tailwind()],
 });
